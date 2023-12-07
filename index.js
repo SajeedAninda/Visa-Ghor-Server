2b66fefae1675682cb0758731cb8df85834df2e5
SajeedAninda <sajaninda@gmail.com>
2023-12-07 23:36:55 +0600

    Created API To update slip prices

--- index.js
+++ index.js
@@ -53,6 +53,16 @@ async function run() {
             res.send(result);
         });
 
+        // GET DATA TO UPDATE SLIP PRICING 
+        app.get("/updateSlipPricing/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await slipPriceCollection.findOne(query);
+            res.send(result);
+        });
+
 
 
 
